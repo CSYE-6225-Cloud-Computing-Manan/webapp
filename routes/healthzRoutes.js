c24ae2472a56244df2aff45180431bd2e351464a
Manan Vijayvargiya <vijayvargiya.m@northeastern.edu>
2024-09-24 19:17:35 -0400

    handled edge cases
    handled edge cases and updated readme

--- routes/healthzRoutes.js
+++ routes/healthzRoutes.js
@@ -3,8 +3,18 @@ const router = express.Router();
 const healthController = require('../controller/healthzController.js');
 
 console.log('healthzRoutes.js');
+
+router.use('/', (request, response, next) => {
+      if(request.method !== 'GET'){
+            response.set('Cache-Control', 'no-cache');
+            console.log('Wrong HTTPS method used');
+            return response.status(405).send();
+      }
+      next();
+});
+
 router.get('/', healthController.healthCheck); //for the get request
 
-router.all('/', healthController.otherHttpsHealthCheck); //rest https methods
+
 
 module.exports = router;
