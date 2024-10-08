714f3c06672b544a71abbc52477e32a28e85c3da
Manan Vijayvargiya <vijayvargiya.m@northeastern.edu>
2024-10-08 16:50:29 -0400

    adding first workflow

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,17 +12,17 @@ router.use('/', (request, response, next) => {
   if (method !== 'GET' && method !== 'POST' && method !== 'PUT') {
     response.set('Cache-Control', 'no-cache');
     console.log('Wrong HTTPS method used in user routes');
-    return response.status(405).send();
+    return response.status(404).send();
   }
 
   if (method === 'POST' && path !== '/') {
     console.log('POST request beyond "/" is not allowed');
-    return response.status(405).send();
+    return response.status(404).send();
   }
 
   if ((method === 'GET' || method === 'PUT') && path !== '/self') {
     console.log(`${method} request beyond '/self' is not allowed`);
-    return response.status(405).send();
+    return response.status(404).send();
   }
 
   next();
